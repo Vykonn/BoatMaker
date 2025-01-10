99bf873cdbdf2cba6f3ee5ff41a2199cda4e0b04
Vykonn <79595583+Vykonn@users.noreply.github.com>
2025-01-10 12:48:41 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -19,33 +19,54 @@ let currentTask;
 self.onmessage = async (event) => {
     pyodide.globals.set("string_data", JSON.stringify(event.data[0]));
     if (event.data[1] == true) {
-        //Final Export Script
+        try {
+        pyodide.runPython(`
+        import numpy as np
+        import json
+        import os
+        from HullParameterization import Hull_Parameterization as HP
+        json_data = json.loads(string_data)
+        values_array = np.array(list(json_data.values()))
+        print(values_array)
+        Hull = HP(values_array)
+        constraints = Hull.input_Constraints()
+        cons = constraints > 0
+        print(sum(cons)) # should be zero
+        mesh = Hull.gen_stl(NUM_WL=200, PointsPerWL=1000, bit_AddTransom = 1, bit_AddDeckLid = 1, namepath = "/mnt/mesh")
+        print(os.listdir('/mnt'))
+        `)
+        let result = pyodide.FS.readFile("/mnt/mesh.stl", { encoding: "binary" });
+        self.postMessage([result, "download"]);
+        }
+        catch(err) {
+            let result = false
+            self.postMessage(result);
+        }
     }
     else {
-    //Real Stuff
-    try {
-    pyodide.runPython(`
-    import numpy as np
-    import json
-    import os
-    from HullParameterization import Hull_Parameterization as HP
-    json_data = json.loads(string_data)
-    values_array = np.array(list(json_data.values()))
-    print(values_array)
-    Hull = HP(values_array)
-    constraints = Hull.input_Constraints()
-    cons = constraints > 0
-    print(sum(cons)) # should be zero
-    mesh = Hull.gen_stl(NUM_WL=50, PointsPerWL=400, bit_AddTransom = 1, bit_AddDeckLid = 1, namepath = "/mnt/mesh")
-    print(os.listdir('/mnt'))
-    `)
-    let result = pyodide.FS.readFile("/mnt/mesh.stl", { encoding: "binary" });
-    self.postMessage(result);
-    }
-    catch(err) {
-        let result = false
+        try {
+        pyodide.runPython(`
+        import numpy as np
+        import json
+        import os
+        from HullParameterization import Hull_Parameterization as HP
+        json_data = json.loads(string_data)
+        values_array = np.array(list(json_data.values()))
+        print(values_array)
+        Hull = HP(values_array)
+        constraints = Hull.input_Constraints()
+        cons = constraints > 0
+        print(sum(cons)) # should be zero
+        mesh = Hull.gen_stl(NUM_WL=50, PointsPerWL=400, bit_AddTransom = 1, bit_AddDeckLid = 1, namepath = "/mnt/mesh")
+        print(os.listdir('/mnt'))
+        `)
+        let result = pyodide.FS.readFile("/mnt/mesh.stl", { encoding: "binary" });
         self.postMessage(result);
-    }
+        }
+        catch(err) {
+            let result = false
+            self.postMessage(result);
+        }
     }
 
 };
